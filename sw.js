83f2807e273f67dfe0e8588ce6ad08dacebb4df4
林定翔 <wesleylin@lindingxiangdeMac-mini.local>
2026-09-04 23:09:07 +0800

    Use white official logo for admin home icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wd-salon-shell-v3';
-const ASSETS=['./index.html','./admin.html','./icons/wesley-dandy-designer-192-v2.png','./icons/wesley-dandy-designer-512-v2.png','./icons/wesley-dandy-admin-192-v2.png','./icons/wesley-dandy-admin-512-v2.png','./manifest.webmanifest','./admin-manifest.webmanifest'];
+const CACHE='wd-salon-shell-v4';
+const ASSETS=['./index.html','./admin.html','./icons/wesley-dandy-designer-192-v2.png','./icons/wesley-dandy-designer-512-v2.png','./icons/wesley-dandy-admin-white-192-v3.png','./icons/wesley-dandy-admin-white-512-v3.png','./manifest.webmanifest','./admin-manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
