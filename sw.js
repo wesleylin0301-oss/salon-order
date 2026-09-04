071fab1b92c98c02c45817fdb8034f66810fb342
林定翔 <wesleylin@lindingxiangdeMac-mini.local>
2026-09-04 22:59:28 +0800

    Add receiving workflow and PWA support

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='wd-salon-shell-v1';
+const ASSETS=['./index.html','./admin.html','./icons/designer-192.png','./icons/designer-512.png','./icons/admin-192.png','./icons/admin-512.png','./manifest.webmanifest','./admin-manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+});
